921300f99a446f403660a52d2b495506e5edc8f7
ABojo <Alex.bojorquez2598@gmail.com>
2023-10-02 19:29:47 -0400

    Added font, base css, and meta data

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Outfit } from "next/font/google";
+import "../sass/index.scss";
+
+const outfit = Outfit({
+  weight: ["300", "500"],
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Entertainment App",
+  description: "An entertainment app.",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={outfit.className}>{children}</body>
+    </html>
+  );
+}
