a598abc23dccf9cc6081e88a40a376a8dab8daf7
ABojo <Alex.bojorquez2598@gmail.com>
2023-10-09 20:18:18 -0400

    Switched JWT package, added verify method, and get user from cookies method
    The jsonwebtoken package depended on packages that could not run on vercel's edge
    runtime, so I switched to using the jose package for signing and verifying JWTs. Also,
    I added a method for verifying JWTs, and I added functionality for pulling the JWT data
    out of request cookies.

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
+import { SignJWT, jwtVerify, decodeJwt } from "jose";
+import { NextRequest } from "next/server";
 
 export async function hashPassword(password: string) {
   return await bcrypt.hash(password, 10);
@@ -10,7 +11,24 @@ export async function passwordIsValid(password: string, hash: string) {
 }
 
 export async function generateJWT(userData: { id: string; username: string }) {
-  const token = await jwt.sign(userData, process.env.JWT_SECRET as string);
+  const token = await new SignJWT(userData)
+    .setProtectedHeader({ alg: "HS256", typ: "JWT" })
+    .sign(new TextEncoder().encode(process.env.JWT_SECRET));
 
   return token;
+}
+
+export async function verifyJWT(token: string) {
+  const { payload } = await jwtVerify(token, new TextEncoder().encode(process.env.JWT_SECRET));
+
+  if (payload) return true;
+
+  return false;
+}
+
+export function getUserFromCookies(req: NextRequest) {
+  const cookie = req.cookies.get("JWT")!;
+  const jwtData = decodeJwt(cookie.value);
+
+  return jwtData;
 }
