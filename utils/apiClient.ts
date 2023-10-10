79eeda3b387fedbcb22c0a5ddffb248fda873e70
ABojo <Alex.bojorquez2598@gmail.com>
2023-10-09 21:33:49 -0400

    Added an API Client
    Added an API client, with only a login method for now, that will be used by the
    front end to make requests to the backend.

---
+++ utils/apiClient.ts
@@ -0,0 +1,10 @@
+const apiClient = {
+  login: async function (username: string, password: string) {
+    const response = await fetch("/api/login", { method: "POST", body: JSON.stringify({ username, password }) });
+    const json = await response.json();
+
+    return json;
+  },
+};
+
+export default apiClient;
