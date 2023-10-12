af50eb7af213e9c89e889180e52f85f9c0010bc5
ABojo <Alex.bojorquez2598@gmail.com>
2023-10-11 22:50:38 -0400

    Added a registration method to the API client

--- utils/apiClient.ts
+++ utils/apiClient.ts
@@ -5,6 +5,12 @@ const apiClient = {
 
     return json;
   },
+  register: async function (username: string, password: string) {
+    const response = await fetch("/api/register", { method: "POST", body: JSON.stringify({ username, password }) });
+    const json = await response.json();
+
+    return json;
+  },
 };
 
 export default apiClient;
