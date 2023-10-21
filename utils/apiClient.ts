82c709dbd57272e633ad23d48e0ffad6693b0e84
ABojo <Alex.bojorquez2598@gmail.com>
2023-10-20 22:31:33 -0400

    Added a method for fetching media to the apiClient

--- utils/apiClient.ts
+++ utils/apiClient.ts
@@ -1,3 +1,8 @@
+interface MediaQuery {
+  category?: "tv" | "movie";
+  title?: string;
+}
+
 const apiClient = {
   login: async function (username: string, password: string) {
     const response = await fetch("/api/login", { method: "POST", body: JSON.stringify({ username, password }) });
@@ -10,6 +15,16 @@ const apiClient = {
     const json = await response.json();
 
     return json;
+  },
+  getMedia: async function (query?: MediaQuery) {
+    let apiRoute = "/api/media";
+
+    if (query) apiRoute += `?${new URLSearchParams({ ...query }).toString()}`;
+
+    const response = await fetch(apiRoute);
+    const json = await response.json();
+
+    return json.data;
   },
 };
 
