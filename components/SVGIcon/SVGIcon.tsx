0f17de56091cb2a3739492e0231e93fa7492d2a4
ABojo <Alex.bojorquez2598@gmail.com>
2023-11-03 23:12:03 -0400

    Added bookmark-empty and bookmark-full to the SVG component

--- components/SVGIcon/SVGIcon.tsx
+++ components/SVGIcon/SVGIcon.tsx
@@ -1,5 +1,5 @@
 interface SVGIconProps {
-  type: "movie" | "tv" | "play";
+  type: "movie" | "tv" | "play" | "bookmark-empty" | "bookmark-full";
   className?: string;
   height?: number;
   width?: number;
@@ -48,6 +48,26 @@ export default function SVGIcon({ type, className, height, width }: SVGIconProps
         >
           <path
             d="M15 0C6.713 0 0 6.713 0 15c0 8.288 6.713 15 15 15 8.288 0 15-6.712 15-15 0-8.287-6.712-15-15-15Zm-3 21V8l9 6.5-9 6.5Z"
+            fill="currentColor"
+          />
+        </svg>
+      );
+    case "bookmark-empty":
+      return (
+        <svg width={12 || width} height={14 || height} xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="m10.518.75.399 12.214-5.084-4.24-4.535 4.426L.75 1.036l9.768-.285Z"
+            stroke="currentColor"
+            stroke-width="1.5"
+            fill="none"
+          />
+        </svg>
+      );
+    case "bookmark-full":
+      return (
+        <svg width={12 || width} height={14 || height} xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M10.61 0c.14 0 .273.028.4.083a1.03 1.03 0 0 1 .657.953v11.928a1.03 1.03 0 0 1-.656.953c-.116.05-.25.074-.402.074-.291 0-.543-.099-.756-.296L5.833 9.77l-4.02 3.924c-.218.203-.47.305-.756.305a.995.995 0 0 1-.4-.083A1.03 1.03 0 0 1 0 12.964V1.036A1.03 1.03 0 0 1 .656.083.995.995 0 0 1 1.057 0h9.552Z"
             fill="currentColor"
           />
         </svg>
