d498029275f45bde26e4f7acef61484708764b3b
ABojo <Alex.bojorquez2598@gmail.com>
2023-11-19 23:57:53 -0500

    Fixed missing JWT error on /me route
    The JWT Cookie  was showing up as undefined when making requests to the /me route in
    production. The cookie is accessible on every other production route. I added a line
    to look at the header entries because the cookie was attached in my browser tools,
    and for a reason I cannot discern, simply calling headers.entries() fixed the
    problem. If I remove the invocation tnen I can no longer  access the cookie.
    I don't know if this is a bug, or if I'm missing something about Next.js.
    
    This problem was isolated to the "/api/me" route. The cookie for the JWT was showing
    up as undefined despite working in my local enviornment and every other route in
    production.

--- app/api/me/route.ts
+++ app/api/me/route.ts
@@ -4,5 +4,9 @@ import { addAdminGuard } from "../../../utils/guards";
 
 export const GET = async function (req: NextRequest) {
   const user = await getCurrentUser(req);
+
+  //this line makes the JWT cookie accessible in production(I have ZERO idea why)
+  req.headers.entries();
+
   return NextResponse.json({ status: "success", data: user });
 };
