7c156546cb30032130389c9ffd2574dfd01304af
ABojo <Alex.bojorquez2598@gmail.com>
2023-11-21 19:54:53 -0500

    Built an auth guard higher order function
    I was unhappy with using middleware to protect routes because I can't look at
    at a specific route handler and determine if it is protected or not. I have to open
    the middleware to find out, which is more confusing, so I decided to create a higher
    order function that will tack an auth guard onto the handlers. I didn't want to copy
    and paste the same auth check into every router handler, so this was the best option
    for readability and maintainability. The auth guard checks if the current user is
    authenticated and then feeds the current user into the route handler if all went
    well.

--- utils/guards.ts
+++ utils/guards.ts
@@ -1,5 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getCurrentUser } from "./auth";
+import UserResponse from "../types/UserResponse";
+
+export function addAuthGuard<T>(
+  routeHandler: (req: NextRequest, user: UserResponse, config?: T) => Promise<NextResponse>
+) {
+  return async function (req: NextRequest, params?: T) {
+    const user = await getCurrentUser(req);
+
+    if (!user) {
+      return NextResponse.json(
+        { status: "error", message: "You must be logged in to perform that action! test" },
+        { status: 401 }
+      );
+    }
+
+    return await routeHandler(req, user, params);
+  };
+}
 
 export function addAdminGuard(routeHandler: (req: NextRequest) => Promise<NextResponse>) {
   return async function (req: NextRequest) {
